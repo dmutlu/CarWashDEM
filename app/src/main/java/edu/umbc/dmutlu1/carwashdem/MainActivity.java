d78c4a3babb2a88478635fc6cb9f678b2647724f
dmutlu <devin.mutlu+github@gmail.com>
2017-11-03 23:03:33 -0400

    Attempted to implement Gradle4.3 in IntelliJ 2017.2.5

--- app/src/main/java/edu/umbc/dmutlu1/carwashdem/MainActivity.java
+++ app/src/main/java/edu/umbc/dmutlu1/carwashdem/MainActivity.java
@@ -1,15 +1,86 @@
 package edu.umbc.dmutlu1.carwashdem;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.*;
+
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity
 {
+    private final double extPrice = 10.99;
+    private final double extIntPrice = 15.99;
+    private final double extPriceDiscount = 8.99;
+    private final double extIntPriceDiscount = 12.99;
+    private int washes;
+    private double custPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final RadioButton radioExt = findViewById(R.id.radioExtOnly);
+        final RadioButton radioExtInt = findViewById(R.id.radioExtInt);
+        final EditText numWash = findViewById(R.id.editText);
+        Button calcBtn = findViewById(R.id.button);
+        NumberFormat currency = NumberFormat.getCurrencyInstance(Locale.getDefault());
+
+        calcBtn.setOnClickListener(v -> {
+            final TextView output = findViewById(R.id.textOut);
+
+            if (numWash.getText().toString().isEmpty())
+            {
+                washes = 0;
+            }
+            else
+            {
+                washes = Integer.parseInt(numWash.getText().toString());
+            }
+
+            if (washes == 0)
+            {
+                Context context = getApplicationContext();
+                CharSequence text = getString(R.string.toast);
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+            else if (radioExt.isSelected())
+            {
+                if (washes < 12)
+                {
+                    custPrice = washes * extPrice;
+                }
+
+                else
+                {
+                    custPrice = washes * extPriceDiscount;
+                }
+
+                output.setVisibility(1);
+                output.setText(currency.format(custPrice));
+
+            }
+            else if (radioExtInt.isSelected())
+            {
+                if (washes < 12)
+                {
+                    custPrice = washes * extIntPrice;
+                }
+
+                else
+                {
+                    custPrice = washes * extIntPriceDiscount;
+                }
+
+                output.setVisibility(1);
+                output.setText(currency.format(custPrice));
+            }
+        });
     }
 }
